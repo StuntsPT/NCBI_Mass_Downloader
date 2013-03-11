0fd4634694ca954ed37ec94b785abcd89662e96a
Francisco Pina Martins <f.pinamartins@gmail.com>
2013-03-11 13:55:54 +0000

    Modified to use a cookie, so downloads can be resumed.

--- NCBI_dl.py
+++ NCBI_dl.py
@@ -6,15 +6,29 @@ from sys import argv
 
 Entrez.email = "f.pinamartins@gmail.com"
 
-handle = Entrez.esearch(db=argv[2],term=argv[1],retmax=1000000)
+handle = Entrez.esearch(db=argv[2],term=argv[1],usehistory="y",retmax=1000000)
 record = Entrez.read(handle)
-IDs = ",".join(record["IdList"])
+handle.close()
 
+count = int(record["Count"])
+IDs = record["IdList"]
+webenv = record["WebEnv"]
+query_key = record["QueryKey"]
 
-handle2 = Entrez.efetch(db=argv[2], id=IDs, rettype="fasta")
+print(count)
+print(len(IDs))
+
+assert count == len(IDs)
+
+batch_size = 1000
 
 outfile = open(argv[3],'w')
-
-outfile.write(handle2.read())
+for start in range(0,count,batch_size):
+    end = min(count,batch_size)
+    print("Going to download record %i to %i") % (start+1, end)
+    fetch_handle = Entrez.efetch(db=argv[2], rettype="fasta", restart=start, retmax=batch_size, webenv=webenv, query_key=query_key)
+    data = fetch_handle.read()
+    fetch_handle.close()
+    outfile.write(data)
 
 outfile.close()
