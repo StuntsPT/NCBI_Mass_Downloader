5665864924afdff4de09828ca449b307dbd99ace
Francisco Pina Martins <f.pinamartins@gmail.com>
2013-03-12 22:11:18 +0000

    corrected a serious typo.

--- NCBI_dl.py
+++ NCBI_dl.py
@@ -23,7 +23,7 @@ outfile = open(argv[3],'w')
 for start in range(0,count,batch_size):
     end = start + batch_size
     print("Going to download record %i to %i of %i") % (start+1, end, count)
-    fetch_handle = Entrez.efetch(db=argv[2], rettype="fasta", restart=start, retmax=batch_size, webenv=webenv, query_key=query_key)
+    fetch_handle = Entrez.efetch(db=argv[2], rettype="fasta", retstart=start, retmax=batch_size, webenv=webenv, query_key=query_key)
     data = fetch_handle.read()
     fetch_handle.close()
     outfile.write(data)
