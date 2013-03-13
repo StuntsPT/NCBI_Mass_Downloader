55d3f50ffc0aa05787c2eb8091ea89a6c1970c25
Francisco Pina Martins <f.pinamartins@gmail.com>
2013-03-13 14:50:46 +0000

    Made the program functional.

--- NCBI_dl.py
+++ NCBI_dl.py
@@ -9,29 +9,45 @@ user_email = "f.pinamartins@gmail.com"
 database = argv[2]
 search_term = argv[1]
 output_file = argv[3]
-
-Entrez.email = user_email
-
-handle = Entrez.esearch(db=database,term=search_term,usehistory="y",retmax=10000000)
-record = Entrez.read(handle)
-handle.close()
-
-count = int(record["Count"])
-IDs = record["IdList"]
-webenv = record["WebEnv"]
-query_key = record["QueryKey"]
-
-assert count == len(IDs)
-
 batch_size = 1000
 
-outfile = open(output_file,'w')
-for start in range(0,count,batch_size):
-    end = start + batch_size
-    print("Going to download record %i to %i of %i") % (start+1, end, count)
-    fetch_handle = Entrez.efetch(db=database, rettype="fasta", retstart=start, retmax=batch_size, webenv=webenv, query_key=query_key)
-    data = fetch_handle.read()
-    fetch_handle.close()
-    outfile.write(data)
 
-outfile.close()
+def NCBI_search(Uemail, DB, ST):
+    #Submit search to NCBI and return the records
+    Entrez.email = Uemail
+    handle = Entrez.esearch(db=DB,term=ST,usehistory="y",retmax=10000000)
+    record = Entrez.read(handle)
+    handle.close()
+
+    return record
+
+def Record_processor(record):
+    #Processes the record into sparate usefull information
+    count = int(record["Count"])
+    IDs = record["IdList"]
+    webenv = record["WebEnv"]
+    query_key = record["QueryKey"]
+
+    assert count == len(IDs)
+
+    return count, IDs, webenv, query_key
+
+def NCBI_fetch(output_file, count, IDs, webenv, query_key, Bsize):
+    #Fetches results from NCBI
+    outfile = open(output_file,'w')
+    if Bsize > count:
+        Bsize = count
+    for start in range(0,count,Bsize):
+        end = start + Bsize
+        print("Downloading record %i to %i of %i") % (start+1, end, count)
+        fetch_handle = Entrez.efetch(db=database, rettype="fasta", retstart=start, retmax=Bsize, webenv=webenv, query_key=query_key)
+        data = fetch_handle.read()
+        fetch_handle.close()
+        outfile.write(data)
+
+    outfile.close()
+
+#Run everything
+rec = NCBI_search(user_email, database, search_term)
+count, IDs, webenv, query_key = Record_processor(rec)
+NCBI_fetch(output_file, count, IDs, webenv, query_key, batch_size)
