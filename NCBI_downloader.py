51ec353c68097ff6470d47238be97baea338a0e8
Francisco Pina Martins <f.pinamartins@gmail.com>
2013-05-30 16:29:51 +0100

    Renamed the files to better reflect the new usage.

---
+++ NCBI_downloader.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python3
+
+#Usage: NCBI_dl.py "user@email-address.com" "Query term" "database" outfile.fasta
+#Or run without arguments for a GUI version.
+
+import sys
+import downloader
+from PyQt4 import QtGui, QtCore
+
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        ##Create widgets
+        #Download button
+        dlbtn = QtGui.QPushButton("Download!", self)
+        dlbtn.setToolTip('Click to start downloading...')
+        dlbtn.clicked.connect(self.statusChange)
+        dlbtn.clicked.connect(self.runOnClick)
+        dlbtn.resize(dlbtn.sizeHint())
+
+        #Quit button
+        qbtn = QtGui.QPushButton("Quit", self)
+        qbtn.setToolTip('Click exit the program...')
+        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+
+        #Progress bar
+        self.progbar = QtGui.QProgressBar(self)
+        #self.pbar.setValue(self."X") Reminder!
+
+        #Status bar
+        self.statusBar().showMessage('Ready')
+
+        #Title lable
+        self.title = QtGui.QLabel(self)
+        self.title.setText("NCBI mass sequence downloader")
+        self.title.setFont(QtGui.QFont("Sans", 16, QtGui.QFont.Bold, True))
+
+        #Email line edit and respective label
+        self.email_line = QtGui.QLineEdit(self)
+        self.email_line.setFixedWidth(220)
+        self.email_label = QtGui.QLabel(self)
+        self.email_label.setText("Email address:")
+
+        #Databases to search and respective label
+        self.databases = QtGui.QComboBox(self)
+        self.databases.addItem("nucleotide")
+        self.databases.addItem("nuccore")
+        self.databases.addItem("nucgss")
+        self.databases.addItem("nucest")
+        self.databases.addItem("protein")
+        self.databases.addItem("genome")
+        self.databases.addItem("popset")
+        self.databases_label = QtGui.QLabel(self)
+        self.databases_label.setText("Database to search:")
+
+        #Search query line and respective label
+        self.search_query = QtGui.QLineEdit(self)
+        self.search_query.setFixedWidth(300)
+        self.search_query_label = QtGui.QLabel(self)
+        self.search_query_label.setText("Search Query:")
+
+        #File management
+        self.save_file_label = QtGui.QLabel(self)
+        self.save_file_label.setText("File Location:")
+
+        self.save_file_line = QtGui.QLineEdit(self)
+        self.save_file_line.setFixedWidth(300)
+
+        self.save_file_button = QtGui.QPushButton("Find...", self)
+        self.save_file_button.resize(dlbtn.sizeHint())
+        self.save_file_button.setToolTip('Click to select file location...')
+        self.save_file_button.clicked.connect(self.fileHandle)
+
+        #self.savefile = QtGui.QFileDialog.getSaveFileName(self, "Save to file...", "", ".fasta")
+
+        #Container widget
+        self.main_widget = QtGui.QWidget(self)
+
+        ##Set layout
+        #Containers
+        self.main_layout = QtGui.QVBoxLayout(self.main_widget)
+        self.main_layout.sizeConstraint = QtGui.QLayout.SetDefaultConstraint
+
+        self.main_widget.setLayout(self.main_layout)
+        self.setCentralWidget(self.main_widget)
+
+        #Box for progress bar
+        self.progressBox = QtGui.QHBoxLayout()
+        self.progressBox.addWidget(self.progbar)
+
+        #Box for search query
+        self.queryBox = QtGui.QHBoxLayout()
+        self.queryBox.addWidget(self.search_query_label)
+        self.queryBox.addWidget(self.search_query)
+        self.queryBox.addStretch(1)
+
+        #Box for email and database
+        self.email_database_box = QtGui.QHBoxLayout()
+        self.email_database_box.addWidget(self.email_label)
+        self.email_database_box.addWidget(self.email_line)
+
+        self.email_database_box.addStretch(1)
+
+        self.email_database_box.addWidget(self.databases_label)
+        self.email_database_box.addWidget(self.databases)
+
+        #Box for file management
+        self.file_box = QtGui.QHBoxLayout()
+        self.file_box.addWidget(self.save_file_label)
+        self.file_box.addWidget(self.save_file_line)
+        self.file_box.addWidget(self.save_file_button)
+
+        #Box for Title
+        self.titlebox = QtGui.QHBoxLayout()
+        self.titlebox.addStretch(1)
+        self.titlebox.addWidget(self.title)
+        self.titlebox.addStretch(1)
+
+        #Box for bottom buttons
+        self.bottomBox = QtGui.QHBoxLayout()
+        self.bottomBox.addStretch(1)
+        self.bottomBox.addWidget(dlbtn)
+        self.bottomBox.addWidget(qbtn)
+
+        #Vertical stack
+        self.main_layout.addLayout(self.titlebox)
+        self.main_layout.addStretch(1)
+        self.main_layout.addLayout(self.email_database_box)
+        self.main_layout.addLayout(self.queryBox)
+        self.main_layout.addLayout(self.file_box)
+        self.main_layout.addStretch(1)
+        self.main_layout.addLayout(self.progressBox)
+        self.main_layout.addLayout(self.bottomBox)
+
+
+        #MainWindow proprieties
+        self.setWindowTitle('NCBI mass downloader')
+        self.setWindowIcon(QtGui.QIcon('assets/Icon.png'))
+
+        #Draw it!
+        self.show()
+
+    def closeEvent(self, event):
+
+        reply = QtGui.QMessageBox.question(self, 'Really quit?',
+            "Are you sure you want to quit?", QtGui.QMessageBox.Yes |
+            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def statusChange(self):
+        if self.statusBar().currentMessage() == "Ready":
+            self.statusBar().showMessage("Downloading...")
+        elif self.statusBar().currentMessage() == "Downloading...":
+            self.statusBar().showMessage("Ready")
+
+    def fileHandle(self):
+        self.savefile = QtGui.QFileDialog.getSaveFileName(self, "Save to file...", "", "Fasta Files (*.fasta);;All Files (*)")
+        self.save_file_line.setText(self.savefile)
+
+    def runOnClick(self):
+        self.email_address = self.email_line.displayText()
+        self.database_to_search = self.databases.currentText()
+        self.search_term = self.search_query.displayText()
+        self.file_to_handle = self.save_file_line.displayText()
+
+        print(self.email_address, self.database_to_search, self.search_term, self.file_to_handle)
+
+        downloader.runEverything(self.email_address, self.database_to_search, self.search_term, self.file_to_handle)
+
+def main():
+
+    if len(sys.argv) < 2:
+        app = QtGui.QApplication(sys.argv)
+        ex = MainWindow()
+        sys.exit(app.exec_())
+    else:
+        downloader.runEverything(sys.argv[1],sys.argv[3],sys.argv[2],sys.argv[4])
+
+
+if __name__ == '__main__':
+    main()
---
+++ downloader.py
@@ -0,0 +1,141 @@
+#!/usb/bin/python3
+
+#Usage: NCBI_dl.py "user@email-address.com" "Query term" "database" outfile.fasta
+#Note that the program will not overwrite the outfile, but rather append sequences to it!
+#Meant to be used from NCBI_dl.py, but can also be used "stand-alone".
+from Bio import Entrez
+from sys import argv
+from shutil import move
+from os import remove, stat
+import re
+
+def NCBI_search(ST, database):
+    #Submit search to NCBI and return the records
+    handle = Entrez.esearch(db=database,term=ST,usehistory="y",retmax=10000000)
+    record = Entrez.read(handle)
+    handle.close()
+
+    return record
+
+def NCBI_post(IDs):
+    #Submit id_list to NCBI via epost and return the records
+    IDs_string = ",".join(IDs)
+    handle = Entrez.epost(database,id=IDs_string,retmax=10000000)
+    record = Entrez.read(handle)
+    handle.close()
+
+    count = len(IDs)
+    webenv = record["WebEnv"]
+    query_key = record["QueryKey"]
+
+    return count, IDs, webenv, query_key
+
+def Record_processor(record):
+    #Processes the record into sparate usefull information
+    count = int(record["Count"])
+    IDs = record["IdList"]
+    webenv = record["WebEnv"]
+    query_key = record["QueryKey"]
+
+    assert count == len(IDs)
+
+    return count, IDs, webenv, query_key
+
+def NCBI_history_fetch(output_file, count, IDs, webenv, query_key, Bsize, Run, database):
+    #Fetches results from NCBI using history
+    try:
+        a = open(output_file,'r')
+        a.close()
+    except:
+        a = open(output_file,'w')
+        a.close()
+    if Run == 1 and stat(output_file).st_size != 0:
+        ReDownloader(output_file, IDs, database)
+    else:
+        outfile = open(output_file,'a')
+        if Bsize > count:
+            Bsize = count
+        for start in range(0,count,Bsize):
+            if start + Bsize < count:
+                end = start + Bsize
+            else:
+                end = count
+            print("Downloading record %i to %i of %i") % (start+1, end, count)
+            #Make sure that even on server errors the program carries on.
+            #If the servers are dead, well, you were not going anywhere anyway...
+            while True:
+                try:
+                    fetch_handle = Entrez.efetch(db=database, rettype="fasta", retstart=start, retmax=Bsize, webenv=webenv, query_key=query_key)
+                    break
+                except:
+                    print(1)
+                    pass
+            data = fetch_handle.read()
+            fetch_handle.close()
+            outfile.write(data)
+
+    outfile.close()
+    ReDownloader(output_file, IDs, database)
+
+def ReDownloader(output_file, IDs, database):
+    #Check for missing sequences:
+    print("Checking for sequences that did not download... Please wait.")
+    ver_IDs = Error_finder(output_file)
+    missing_IDs = set()
+    for i in IDs:
+        if i not in ver_IDs:
+            missing_IDs.add(i)
+    IDs = missing_IDs #Improve performance on subsequent runs
+    if len(missing_IDs) == 0:
+        print("All sequences were downloaded correctly. Good!")
+        quit("Program finished without error.")
+    else:
+        print("%s sequences did not download correctly (or at all). Retrying...") %(len(missing_IDs))
+        count, IDs, webenv, query_key = NCBI_post(IDs)
+        NCBI_history_fetch(output_file, count, IDs, webenv, query_key, 1000, 2, database)
+
+
+def Error_finder(output_file):
+    #Looks for errors in the output fasta and retruns a list of necessary retries
+    temp_file = output_file + ".tmp"
+    move(output_file, temp_file)
+    original_file = open(temp_file,'r')
+    new_file = open(output_file,'w')
+    verified_IDs = set()
+
+    for lines in original_file:
+        if lines.startswith(">"):
+            ID = re.search("gi\|.*?\|",lines).group(0)[3:-1]
+            verified_IDs.add(ID)
+            new_file.write("\n" + lines) #TODO: remove first empty line from file
+        elif lines.strip().startswith("<") or lines.startswith("\n"):
+            pass
+        else:
+            new_file.write(lines)
+
+    original_file.close()
+    new_file.close()
+    remove(temp_file)
+
+    return verified_IDs
+
+def main():
+    #Set vars or get them from the GUI:
+    user_email = argv[1]
+    database = argv[3]
+    search_term = argv[2]
+    output_file = argv[4]
+
+    runEverything(user_email, database, search_term, output_file)
+
+def runEverything(user_email, database, search_term, output_file):
+    #Run the functions in order
+    batch_size = 1000
+    Entrez.email = user_email
+
+    rec = NCBI_search(search_term, database)
+    count, IDs, webenv, query_key = Record_processor(rec)
+    NCBI_history_fetch(output_file, count, IDs, webenv, query_key, batch_size, 1, database)
+
+if __name__ == '__main__':
+    main()
