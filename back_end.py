22f4b1081b2aeae5ed6ffcfecb5b1204b038deff
Francisco Pina Martins <stunts@pinamartins.com>
2021-04-01 03:27:13 +0100

    Simplifies the code and fixes #23

--- back_end.py
+++ back_end.py
@@ -19,27 +19,24 @@
 
 import sys
 import re
-import requests
-import json
 from os import stat
 from time import sleep
 
-import Entrez
+import requests
 
 
-class Downloader(object):
+class Downloader():
     def __init__(self, database, term, outfile, gui):
         self.database = database
         self.term = term
         self.outfile = outfile
         self.gui = gui
-        self.terminated = False
         self.failures = [[], 0]
-        self.retry_threshold = 5
         super(Downloader, self).__init__()
         self.run = 0
-        self.webenv = ""
-        self.query_key = ""
+        self.api_key = ""
+        self.original_count = 0
+
 
     def ncbi_search(self, database, term, history="y", retmax=0):
         """
@@ -52,7 +49,8 @@ class Downloader(object):
                          "retmode": "json",
                          "usehistory": history,
                          "idtype": "acc",
-                         "retmax": retmax}
+                         "retmax": retmax,
+                         "api_key": self.api_key}
 
         handle = requests.get(url, params=search_params)
         record["qkey"] = handle.json()["esearchresult"]["querykey"]
@@ -60,157 +58,61 @@ class Downloader(object):
         record["count"] = int(handle.json()["esearchresult"]["count"])
         record["accn"] = handle.json()["esearchresult"]["idlist"]
 
-        return record
-
-
-    def record_processor(self, record):
-        """
-        Splits the record returned by Entrez into sparate variables and returns
-        them.
-        """
-        count = int(record["Count"])  # Int
-        webenv = record["WebEnv"]  # String
-        query_key = record["QueryKey"]  # String
-
-        if count == 0 and self.gui == 0:
+        if record["count"] == 0 and self.gui == 0:
             sys.exit("Your serch query returned no results!")
-
-        elif count == 0:
+        elif record["count"] == 0 and self.gui == 1:
             self.no_match.emit("Your serch query returned no results!")
             return None
 
-        return count, webenv, query_key
+        return record
 
-    def main_organizer(self, count, b_size):
+
+    def main_organizer(self, count, b_size, query_key, webenv):
         """
         Defines what tasks need to be performed, handles NCBI server errors and
         writes the sequences into the outfile.
         """
         try:
-            if self.run == 1 and stat(self.outfile).st_size != 0:
-                self.re_downloader(b_size, count)
-                return None
+            stat(self.outfile).st_size != 0
+            missing_accns = self.missing_checker(count)
+            count = len(missing_accns)
         except OSError:
-            pass
+            missing_accns = None
 
-        outfile = open(self.outfile, 'a')
-        if b_size > count:
-            b_size = count
-        for start in range(0, count, b_size):
-            if self.terminated is False:
-                if start + b_size < count:
-                    end = start + b_size
-                else:
-                    end = count
-                print("Downloading record %i to %i of %i" % (start + 1, end,
-                                                             count))
-
-                if self.gui == 1:
-                    self.max_seq.emit(count)
-                    self.prog_data.emit(end)
+        self.actual_downloader(count, b_size, missing_accns, query_key, webenv)
 
 
-                fetch_func = self.fetch_by_history
-                fetch_args = start, b_size
-
-                # Make sure that the program carries on despite server
-                # "hammering" errors.
-                attempt = 0
-                while self.terminated is False:
-                    try:
-                        data = fetch_func(*fetch_args)
-                        if data.startswith("<?"):
-                            raise ValueError("NCBI server error.")
-                        else:
-                            data = data.replace("\n\n", "\n")
-                            break
-                    except ValueError:
-                        if attempt < 5:
-                            print("NCBI is retuning XML instead of sequence "
-                                  "data. Trying the same chunk again in 8\'\'.")
-                            attempt += 1
-                            sleep(8)
-                        else:
-                            print("Too many errors in a row. Let's make a "
-                                  " larger 20\'\' pause and try again.")
-                            attempt = 0
-                            sleep(20)
-                if self.terminated is False:
-                    outfile.write(data)
-
-        outfile.close()
-        if self.terminated is False:
-            self.re_downloader(b_size, count)
-
-    def re_downloader(self, b_size, count):
+    def missing_checker(self, count):
         """
         Checks if any sequences did not download correctlly.
-        If discrepancies between the downloaded sequences and the IdList are
-        found, a new IdList is generated, based on what is missing and an
+        If discrepancies between the downloaded sequences and the Accesions are
+        found, a new missing sequences list is generated and an
         attempt is made to fetch the missing sequences.
         """
-        if self.terminated is True:
-            return
+        print("Checking for sequences that did not download... Please wait.")
+        ver_ids = self.fasta_parser(self.outfile)
+        ncbi_accn_set = set(self.ncbi_search(self.database,
+                                             self.term,
+                                             "n",
+                                             self.original_count)["accn"])
+
+        # Remove any Master records from the accn set:
+        # See https://www.biostars.org/p/305310/#305317
+        master_records = set()
+        for accn in ncbi_accn_set:
+            if bool(re.search('[A-Z]{4}0+(\.\d){0,}$', accn)):
+                master_records.add(accn)
+                print("WARNING: Master record found and "
+                      "removed: %s." % (accn))
+        ncbi_accn_set = ncbi_accn_set - master_records
+
+        if ver_ids == ncbi_accn_set:
+            self.finish(success=True)
+
         else:
-            print("Checking for sequences that did not download... Please "
-                  "wait.")
-            ver_ids = self.fasta_parser(self.outfile)
-            print("Getting an accession number list to compare with the local "
-                  "file... Please wait")
-            ncbi_accn_set = set(self.ncbi_search(self.database,
-                                                 self.term, "n", count)["accn"])
-            # Remove any Master records from the accn set:
-            master_records = set()
-            for accn in ncbi_accn_set:
-                if bool(re.search('[A-Z]{4}0+(\.\d){0,}$', accn)):
-                    master_records.add(accn)
-                    print("WARNING: Master record found and "
-                          "removed: %s." % (accn))
-            ncbi_accn_set = ncbi_accn_set - master_records
+            missing_ids = ncbi_accn_set - ver_ids
 
-            if ver_ids == ncbi_accn_set:
-                print("All sequences were downloaded correctly. Good!")
-                if self.gui == 0:
-                    sys.exit("Program finished without error.")
-                else:
-                    self.finished.emit("Download finished successfully!")
-            else:
-                missing_ids = ncbi_accn_set - ver_ids
-
-                if self.failures[0] != missing_ids:
-                    self.failures[0] = missing_ids
-                    self.failures[1] = 0
-                else:
-                    self.failures[1] += 1
-                numb_missing = len(missing_ids)
-                if numb_missing == 0:
-                    print("All sequences were downloaded correctly. Good!")
-                    if self.gui == 0:
-                        sys.exit("Program finished without error.")
-                    else:
-                        self.finished.emit("Download finished successfully!")
-
-                elif self.failures[1] < self.retry_threshold:
-                    print("%s sequences did not download correctly (or at all)."
-                          " Retrying..." % (numb_missing))
-                    self.run = 2
-                    self.main_organizer(numb_missing, b_size)
-                else:
-                    print("NOTICE: After %s retries, not all sequences were "
-                          "downloaded correctly.=-(" % (self.retry_threshold))
-                    print("A list of failed downloads can be found on %s.failed"
-                          % (self.outfile))
-                    fail_log = open(self.outfile + ".failed", "w")
-                    for i in missing_ids:
-                        fail_log.write(i + "\n")
-                    fail_log.close()
-                    if self.gui == 0:
-                        sys.exit("Program finished without error.")
-                    else:
-                        self.finished.emit("Download finished with some "
-                                           "failures!\nPlease check the file "
-                                           "%s.failed for a detailed "
-                                           "list." % (self.outfile))
+            return missing_ids
 
 
     def fasta_parser(self, target_file):
@@ -228,37 +130,34 @@ class Downloader(object):
         target_handle.close()
         return verified_ids
 
-    def fetch_by_accn(self, accns, b_size):
-        """
-        Fetches NCBI data based on the IDs, rather than a search query. Returns
-        the data handle string.
-        """
-        # id_handle = Entrez.efetch(db=self.database,
-        #                           id=ids,
-        #                           rettype="fasta",
-        #                           retmode="text",
-        #                           retmax=b_size)
-        # data = id_handle.read()
-        # id_handle.close()
 
-        return
-
-    def fetch_by_history(self, start, b_size):
+    def use_efetch(self, start, b_size, webenv = "", query_key = "", accn=None):
         """
-        Fetches NCBI data based on the provided search query. Returns the data
-        handle string.
+        Fetches NCBI data based on the provided search query or acession
+        numbers. Returns the fasta string (or XML in case of erros).
         """
-        hist_handle = Entrez.efetch(db=self.database,
-                                    retstart=start,
-                                    rettype="fasta",
-                                    retmode="text",
-                                    retmax=b_size,
-                                    webenv=self.webenv,
-                                    query_key=self.query_key)
-        data = hist_handle.read()
-        hist_handle.close()
+        # record = {}
+        url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+        search_params = {"db": self.database,
+                         "retmode": "text",
+                         "rettype": "fasta",
+                         "api_key": self.api_key}
 
-        return data
+        if accn is not None:
+            accn = ",".join(accn)
+            search_params["id"] = accn
+            handle = requests.post(url, params=search_params)
+
+        elif webenv != "":
+            search_params["WebEnv"] = webenv
+            search_params["query_key"] = query_key
+            search_params["retstart"] = start
+            search_params["retmax"] = b_size
+            search_params["term"] = self.term
+            handle = requests.get(url, params=search_params)
+
+        return handle.text
+
 
     def translate_genome(self, count):
         """
@@ -286,26 +185,99 @@ class Downloader(object):
         return nuc_acc_list
 
 
+    def finish(self, success, msg=""):
+        """
+        Run this funcion whenever downloads are finished.
+        """
+        if success is True:
+            print("All sequences were downloaded correctly. Good!")
+            if self.gui == 0:
+                sys.exit("Program finished without error.")
+            else:
+                self.finished.emit("Download finished successfully!")
+        else:
+            if self.gui == 0:
+                sys.exit("Program finished with some failures.\n" + msg)
+            else:
+                self.finished.emit("Download finished with some "
+                                   "failures!\nPlease check the file "
+                                   "%s.failed for a detailed "
+                                   "list." % (self.outfile))
+
+
+    def actual_downloader(self, count, b_size, missing_accns, query_key, webenv):
+        """
+        Manages downloads
+        """
+        outfile = open(self.outfile, 'a')
+        if b_size > count:
+            b_size = count
+        for start in range(0, count, b_size):
+            if start + b_size < count:
+                end = start + b_size
+            else:
+                end = count
+            print("Downloading record %i to %i of %i" % (start + 1, end, count))
+
+            if self.gui == 1:
+                self.max_seq.emit(count)
+                self.prog_data.emit(end)
+
+            # Make sure that the program carries on despite server
+            # "hammering" errors.
+            attempt = 0
+            while attempt < 5:
+                try:
+                    if missing_accns is None:
+                        data = self.use_efetch(start,
+                                               b_size,
+                                               webenv=webenv,
+                                               query_key=query_key)
+                    else:
+                        data = self.use_efetch(start,
+                                               b_size,
+                                               accn = missing_accns)
+                    if data.startswith("<?"):
+                        raise ValueError("NCBI server error.")
+                    data = data.replace("\n\n", "\n")
+                    break
+                except ValueError:
+                    print("NCBI is retuning XML instead of sequence "
+                          "data. Trying the same chunk again in 8\'\'.")
+                    attempt += 1
+                    sleep(8)
+            else:
+                self.finish(False, "Too many errors in a row."
+                            "Please wait a few minutes and try "
+                            "again. (If you use the same output "
+                            "file, your download will resume from "
+                            "where it left off.")
+
+            outfile.write(data)
+
+        outfile.close()
+        self.main_organizer(count, b_size, query_key, webenv)
+
+
     def run_everything(self):
         """
         Run the functions in order.
         """
         batch_size = 3000
-        Entrez.email = "frmartins@ciencias.ulisboa.pt"
-        Entrez.api_key = "bbceccfdf97b6b7e06e93c918e010f1ecf09"
+        self.api_key = "bbceccfdf97b6b7e06e93c918e010f1ecf09"
         self.run = 1
 
         record = self.ncbi_search(self.database, self.term)
         count = record["count"]
-        self.query_key = record["qkey"]
-        self.webenv = record["webenv"]
+        self.original_count = count
 
         if self.database == "genome":
             ids = self.translate_genome(count)
             count = len(ids)
             self.database = "nucleotide"
             self.run = 2
-        self.main_organizer(count, batch_size)
+
+        self.main_organizer(count, batch_size, record["qkey"], record["webenv"])
 
 
 def main():
